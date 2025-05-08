ce70d4d17df7275a736d4effafb256db18d2f800
Pyx <git@notpyx.me>
2025-05-07 22:52:47 -0400

    hjgfhgjkigjhfijhfgi

--- AOS/plugins/middleware/__init__.py
+++ AOS/plugins/middleware/__init__.py
@@ -203,7 +203,7 @@ class Logger(BaseHTTPMiddleware):
             f"Code {res.status_code} ({HTTPStatus(res.status_code).phrase})",
             str(res.status_code).startswith("2") and 32 or 31,
             time.time() - t,
-            f"PlaceID: {req.headers.get("Roblox-Id") or "Not a Roblox place"}\n\nRatelimiting data: {len(limited_ips[cf_ip])}/{globals.security["ratelimiting"]["max_reqs"]} used",
+            f"PlaceID: {req.headers.get("Roblox-Id") or 'Not a Roblox place'}\n\nRatelimiting data: {len(limited_ips[cf_ip])}/{globals.security["ratelimiting"]["max_reqs"]} used",
             req.method,
         )
 
