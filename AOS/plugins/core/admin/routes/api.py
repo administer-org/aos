56acd923106523de7a94365c5763782e47745352
Pyx <git@notpyx.me>
2025-07-28 18:13:55 -0400

    unsurance & patches

--- AOS/plugins/core/admin/routes/api.py
+++ AOS/plugins/core/admin/routes/api.py
@@ -1,5 +1,6 @@
 # pyxfluff 2025
 
+import re
 import time
 import bcrypt
 import nanoid
@@ -10,6 +11,11 @@ from fastapi.responses import JSONResponse
 from AOS.plugins.database import get_web_database
 
 db = get_web_database()
+
+
+def validate(password):
+    pattern = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$"
+    return re.match(pattern, password) is not None
 
 
 class AdminRoutes:
@@ -31,12 +37,12 @@ class AdminRoutes:
             if user is None:
                 return JSONResponse(
                     {"code": 401, "data": "Your account was not found."},
-                    status_code=401
+                    status_code=401,
                 )
-            elif user["inactive"]:
+            elif user.get("inactive", False):
                 return JSONResponse(
                     {"code": 401, "data": "Your account has been deactivated."},
-                    status_code=401
+                    status_code=401,
                 )
             elif not bcrypt.checkpw(
                 data["password"].encode(), user["password"].encode()
@@ -50,45 +56,111 @@ class AdminRoutes:
                 "id": nanoid.generate(),
                 "creation": time.time(),
                 "ip": req.headers.get("CF-Connecting-IP"),
-                "browser": req.headers.get("User-Agent")
+                "browser": req.headers.get("User-Agent"),
+                "expiry": data["stayLoggedIn"] and 62899200 or time.time() + 86400 # 2 years is ample time
             }
 
             user["sessions"].append(f"{user["username"]}-{session["id"]}")
+            user["seen"] = time.time()
 
             db.set(data["username"], user, db.USERS)
             db.set(f"{user["username"]}-{session["id"]}", session, db.SESSIONS)
 
-            resp.set_cookie(
-                key="AOS_-SessionAuth",
-                value=session["id"],
-                max_age=data["stayLoggedIn"] and None or 86400,
-                httponly=True,
-                samesite="strict",
-                secure=True
-            )
-
-            return JSONResponse(
+            response = JSONResponse(
                 {"code": 200, "data": "Redirecting..."}, status_code=200
             )
 
+            response.set_cookie(
+                key="AOS_-SessionAuth",
+                value=f"{user["username"]}-{session["id"]}",
+                max_age=data["stayLoggedIn"] and 62899200 or 86400,
+                httponly=True,
+                samesite="strict",
+                secure=False
+            )
+
+            return response
+
         @self.router.post("/signup")
         async def signup(req: Request):
+            db.set(
+                "abc",
+                {
+                    "uses": 0,
+                    "max_uses": 1,
+                    "users": [],
+                    "expiry": 99999999999999,
+                    "creator": "system",
+                },
+                db.SIGNUP_TOKENS,
+            )
             data = await req.json()
             token = db.get(data["signup_token"], db.SIGNUP_TOKENS)
 
+            data["username"] = data["username"].strip()
+
             if not token:
                 return JSONResponse(
-                    {"code": 401, "data": "Invalid signup token"}, status_code=401
+                    {"code": 400, "data": "Invalid signup token"}, status_code=400
                 )
             elif token["uses"] >= token["max_uses"]:
                 return JSONResponse(
-                    {"code": 401, "data": "This token may not be used anymore"},
-                    status_code=401
+                    {"code": 400, "data": "This token may not be used anymore"},
+                    status_code=400,
                 )
             elif token["expiry"] <= time.time():
                 return JSONResponse(
-                    {"code": 401, "data": "This token is expired."}, status_code=401
+                    {"code": 400, "data": "This token is expired."}, status_code=400
                 )
+
+            # holy validation
+            if db.get(data["username"], db.USERS) is not None:
+                return JSONResponse(
+                    {"code": 400, "data": "That username is taken."}, status_code=400
+                )
+            elif not validate(data["password"]):
+                return JSONResponse(
+                    {
+                        "code": 400,
+                        "data": "That password is too weak. Please make sure it has one upper and lowercase letter, one digit, has one special character, and is 8 characters long.",
+                    },
+                    status_code=400,
+                )
+            elif data["username"] in ["system", "aos", "administer"]:
+                return JSONResponse(
+                    {"code": 400, "data": "That username is reserved."}, status_code=400
+                )
+            elif len(data["username"]) <= 3:
+                return JSONResponse(
+                    {"code": 400, "data": "That username is too short."},
+                    status_code=400,
+                )
+            elif len(data["username"]) >= 30:
+                return JSONResponse(
+                    {"code": 400, "data": "That username is too long."}, status_code=400
+                )
+            elif data["username"] in data["password"]:
+                return JSONResponse(
+                    {"code": 400, "data": "Your username can't contain your password."},
+                    status_code=400,
+                )
+            elif not re.match(r"^[a-zA-Z0-9_.!,\-]+$", data["username"]):
+                return JSONResponse(
+                    {
+                        "code": 400,
+                        "data": "Please keep your username limited to standard latin characters.",
+                    },
+                    status_code=400,
+                )
+            elif not re.match(r"^[^@]+@[^@]+\.[^@]+$", data["email"]):
+                return JSONResponse(
+                    {"code": 400, "data": "That is not a real email."}, status_code=400
+                )
+
+            token["uses"] += 1
+            token["users"].append(data["username"])
+
+            db.set(data["signup_token"], token, db.SIGNUP_TOKENS)
 
             db.set(
                 data["username"],
@@ -103,8 +175,13 @@ class AdminRoutes:
                     "created": time.time(),
                     "creation_ip": req.headers.get("CF-Connecting-IP"),
                     "sessions": [],
-                    "permissions": []
-                }
+                    "permissions": [],
+                    "inactive": False,
+                    "seen": -1,
+                },
+                db.USERS,
             )
 
-            return JSONResponse({"code": 200, "data": "Done"}, status_code=200)
+            return JSONResponse(
+                {"code": 200, "data": "Created, redirecting..."}, status_code=200
+            )
--- AOS/plugins/core/admin/static/js/login.js
+++ AOS/plugins/core/admin/static/js/login.js
@@ -38,31 +38,47 @@ if (loginForm != null) {
           respJSON["code"] == 200 ? "good" : "bad";
 
         document.querySelector("#login button").classList.remove("shimmer");
+
+        if (respJSON["code"] == 200)
+          window.location.href = "/a/";
       });
   });
 } else {
   // Signup
-  loginForm.addEventListener("submit", (e) => {
+  signupForm.addEventListener("submit", (e) => {
     e.preventDefault();
 
     let data = {};
-    new FormData(loginForm).forEach((value, key) => {
+    new FormData(signupForm).forEach((value, key) => {
       data[key] = value;
     });
 
-    document.querySelector("#login button").classList.add("shimmer");
+    document.querySelector("#signup button").classList.add("shimmer");
 
-    fetch("/admin/signup", {
-      method: "POST",
-      body: JSON.stringify(data)
-    })
-      .then((reply) => reply.json())
-      .then((respJSON) => {
-        document.getElementById("login-feedback").innerText = respJSON["data"];
-        document.getElementById("login-feedback").className =
-          respJSON["code"] == 200 ? "good" : "bad";
+    try {
+      fetch("/admin/signup", {
+        method: "POST",
+        body: JSON.stringify(data)
+      })
+        .then((reply) => reply.json())
+        .then((respJSON) => {
+          document.getElementById("login-feedback").innerText =
+            respJSON["data"];
+          document.getElementById("login-feedback").className =
+            respJSON["code"] == 200 ? "good" : "bad";
 
-        document.querySelector("#login button").classList.remove("shimmer");
-      });
+          document.querySelector("#signup button").classList.remove("shimmer");
+
+          if (respJSON["code"] == 200)
+            window.location.href = "/a/login?reason=created";
+        });
+    } catch (e) {
+      document.getElementById(
+        "login-feedback"
+      ).innerText = `Something went wrong: ${e}`;
+      document.getElementById("login-feedback").className = "bad";
+
+      document.querySelector("#signup button").classList.remove("shimmer");
+    }
   });
 }
