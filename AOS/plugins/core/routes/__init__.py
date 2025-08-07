12e90ae78c0816ff073cf89986da0abcd016d209
Pyx <git@notpyx.me>
2025-08-07 19:29:43 -0400

    pyproject changes

--- AOS/plugins/core/routes/__init__.py
+++ AOS/plugins/core/routes/__init__.py
@@ -1,5 +1,7 @@
 # pyxfluff 2025
 
+import importlib.util
+
 from .frontend import Frontend
 from .backend import BackendAPI
 from .public import PublicAPI
@@ -9,15 +11,15 @@ from ..admin import AdminRoutes, AdminFrontend
 from AOS import globals, app
 from time import sleep
 
-import AOS.plugin_loader
-import AOS
+import AOS.plugin_loader as plugin_loader
+from AOS.deps import il
 
 while app is None:
     sleep(0)  # wait for fastapi
 
 # Load required dependencies
 for plugin in ["middleware"]:
-    AOS.plugin_loader.load_plugin(plugin, "")
+    plugin_loader.load_plugin(plugin, "")
 
 
 backend_api = BackendAPI(app)
@@ -31,16 +33,21 @@ app.include_router(backend_api.router, prefix="/api")
 app.include_router(backend_api.asset_router, prefix="/api")
 app.include_router(public_api.router, prefix="/pub")
 
-# Mount Admin API
+# Mount Admin API if we are installed with the `admin` optional
 
-admin_api = AdminRoutes(app)
-admin_frontend = AdminFrontend(app)
+if importlib.util.find_spec("bcrypt"):
+    admin_frontend = AdminFrontend(app)
+    admin_api = AdminRoutes(app)
 
-admin_api.mount_api()
-admin_frontend.mount()
+    admin_frontend.mount()
+    admin_api.mount_api()
 
-app.include_router(admin_api.router, prefix="/admin")
-app.include_router(admin_frontend.router, prefix="/a")
+    app.include_router(admin_frontend.router, prefix="/a")
+    app.include_router(admin_api.router, prefix="/admin")
+else:
+    il.cprint("[!] You did not install with `uv pip install .[server], we can't serve the admin interface!", 31)
+
+
 
 frontend = Frontend(app)
 frontend.initialize_frontend()
