09d4a2bfc4e3c53244319ce23c9e1945e3a09055
Pyx <git@notpyx.me>
2025-10-26 23:08:52 -0400

    add --version command, move startup warning

--- AOS/__main__.py
+++ AOS/__main__.py
@@ -7,9 +7,9 @@ import AOS.plugin_loader as Plugin
 from AOS import AOSError, globals as var
 from AOS.utils import logging as logging
 
-from sys import argv
 from time import time
 from pathlib import Path
+from sys import argv, version
 from rich.console import Console
 
 import logging as Pythonlogging
@@ -82,7 +82,6 @@ def help_command():
 
 if "--nobox" not in argv:
     il.box(85, "Administer AOS", f"v{var.version}")
-    il.cprint(f"AOS webserver starting up at {time()}", 34)
 
 try:
     _ = argv[1]
@@ -96,6 +95,10 @@ match argv[1]:
         help_command()
 
         raise AOSError("")
+    case "--version":
+        print(f"AOS/{var.version}\nPython/{version}")
+
+        exit(0)
     case _:
         pass
 
@@ -128,4 +131,5 @@ for config in plugin_list.values():
 
 
 if AOS.app is not None and not bypass_fastapi:
+    il.cprint(f"AOS webserver starting up at {time()}", 34)
     AOS.run_server()
